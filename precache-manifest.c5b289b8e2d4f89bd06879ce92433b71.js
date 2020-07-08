bf31f2174e7c4391f8782ab14d0f5c04aec46eac
Omar Ojeda <omarsilva.ojeda@gmail.com>
2020-07-08 03:54:55 -0500

    Updates

---
+++ precache-manifest.c5b289b8e2d4f89bd06879ce92433b71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b619a166e2b2f90580de0464cd21a0ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcffa0718c205eadfcc8",
+    "url": "/static/js/2.0998dff5.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.0998dff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761e68a693a5fec64e2e",
+    "url": "/static/js/main.9b582453.chunk.js"
+  },
+  {
+    "revision": "a4e169975201acec93b2",
+    "url": "/static/js/runtime-main.a8da2d87.js"
+  }
+]);
